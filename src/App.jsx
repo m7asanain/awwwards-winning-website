97bf01471adf30928eaecb480591a2345f684e52
Mustafa Hasanain <m7asanain@gmail.com>
2025-04-08 19:30:46 +0300

    design hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
-    <main>
-      <h1 className="text-5xl text-violet-300 font-bold">
-        Welcome to Awwwards website
-      </h1>
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Hero />
+      <section className="z-0 min-h-screen bg-blue-500" />
     </main>
   );
 };
