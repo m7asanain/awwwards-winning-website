a703b9c7f427738099510c71005028eeb1238b7e
Mustafa Hasanain <m7asanain@gmail.com>
2025-04-10 13:58:10 +0300

    design features section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import { useState, useRef, useEffect } from "react";
 import Button from "./Button";
-import { TiLocation, TiLocationArrow } from "react-icons/ti";
+import { TiLocationArrow } from "react-icons/ti";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
